47aa6bb7be9f13ea8fca0b89909e7c0b475e2a6f
maulidiya-fitri <fitrimaulidiya5@gmail.com>
2022-09-26 18:39:30 +0700

    first commit

---
+++ application/controllers/Contoh1.php
@@ -0,0 +1,12 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1>Perkenalkan</h1>";
+        echo "Nama saya Maulidiya Fitri Ainurrahma
+        Saya tinggal di Subang
+        Olahraga yang saya sukai adalah
+        Bulutangkis";
+    }
+}
