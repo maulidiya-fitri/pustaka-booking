486552dc35bd5a8c7a5b28c72d19212860432705
maulidiya-fitri <fitrimaulidiya5@gmail.com>
2022-10-07 16:22:03 +0700

    add v_about

---
+++ application/views/v_about.php
@@ -0,0 +1,23 @@
+<section>
+    <h1><?php echo $judul ?></h1>
+    <h4>Nama</h4>
+    <ul type="disc">
+        <li>Nama Depan : Maulidiya Fitri</li>
+        <li>Nama Belakang : Ainurrahma</li>
+    </ul>
+    <h4>Alamat</h4>
+    <ul type="none">
+        <li> Jalan A.Yani Gg Bintang No 77</li>
+    </ul>
+
+    <h4>Tempat Lahir</h4>
+    <ul type="none">
+        <li>Jakarta</li>
+    </ul>
+
+    <h4>Olah Raga Favorit</h4>
+    <ul type="square">
+        <li>Bulutangkis</li>
+        <li>Volly</li>
+    </ul>
+</section>
